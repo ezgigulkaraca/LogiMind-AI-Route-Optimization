bda915cf5f31ea69e978ad76e5db844adc9d024c
ezgigulkaraca <ezgigulkaraca@gmail.com>
2026-05-17 15:37:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="API_KEYİN")
+genai.configure(api_key="AIzaSyAXA7edsGE5ggJrQo5wT2CO2pe2BSbEoKc")
 
 for m in genai.list_models():
     print(m.name)
